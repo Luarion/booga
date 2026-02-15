a74dd99f8dbba5b48168be5125f4f6ad8db102b1
Luarion <raulgamallo@gmail.com>
2026-02-15 14:26:44 +0000

    Added dynamic module ussage for elisya

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -1,11 +1,18 @@
 import { Elysia } from "elysia";
 
 // Module imports
-import * as m from "./modules/index";
+import * as modules from "./modules/index";
 
 const api = new Elysia();
 
-api.use(m.users.default);
+// Load all defined modules
+Object.values(modules).forEach((m) => {
+  if (m.default instanceof Elysia) {
+    const prefix = m.default.config.prefix;
+    console.debug("Loading: " + prefix);
+    api.use(m.default);
+  }
+});
 
 api.listen(3000);
 
