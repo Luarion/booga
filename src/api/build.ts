fb2fa02ef506fb2d0f2cefe1cc6a5c54013a167c
Luarion <raulgamallo@gmail.com>
2026-03-03 19:01:10 +0000

    Add build script for bundling and compiling API

---
+++ src/api/build.ts
@@ -0,0 +1,30 @@
+const entrypoint = "./src/index.ts";
+const outdir = "./dist";
+
+const bundle = await Bun.build({
+  entrypoints: [entrypoint],
+  outdir: outdir,
+  target: "bun",
+  minify: true,
+});
+
+if (!bundle.success) {
+  console.error("Error generando el bundle JS:", bundle.logs);
+  process.exit(1);
+}
+
+const binary = await Bun.build({
+  entrypoints: [entrypoint],
+  compile: {
+    outfile: `${outdir}/api`,
+  },
+});
+
+if (!binary.success) {
+  console.error("Error generando el binario:", binary.logs);
+  process.exit(1);
+}
+
+console.info("Success:");
+console.info(`- JS Bundle: ${bundle.outputs[0]?.path}`);
+console.info(`- Binary: ${binary.outputs[0]?.path}`);
