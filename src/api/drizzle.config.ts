b4ac7abd50f49f4c6f4193b74ec5571aa7af5648
Luarion <raulgamallo@gmail.com>
2026-03-10 16:13:47 +0000

    Fixed .env related pathing

--- src/api/drizzle.config.ts
+++ src/api/drizzle.config.ts
@@ -1,11 +1,17 @@
-import "dotenv/config";
+import { config } from "dotenv";
+import { resolve } from "path";
 import { defineConfig } from "drizzle-kit";
+
+config({ path: resolve(__dirname, "../../.env") });
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "postgresql://booga:booga@db/booga",
+    host: "db",
+    user: process.env.POSTGRES_USER!,
+    password: process.env.POSTGRES_PASSWORD!,
+    database: process.env.POSTGRES_DB!,
   },
 });
