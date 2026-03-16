d0f589ed651247c4bf0028ec41af5cf635df8eb3
Luarion <raulgamallo@gmail.com>
2026-03-16 22:56:18 +0000

    Biome check

--- src/ui/src/lib/eden.ts
+++ src/ui/src/lib/eden.ts
@@ -1,4 +1,4 @@
-import { edenTreaty } from "@elysiajs/eden";
 import type { Server } from "@booga/api/src/server";
+import { edenTreaty } from "@elysiajs/eden";
 
 export default edenTreaty<Server>("http://localhost:3000");
