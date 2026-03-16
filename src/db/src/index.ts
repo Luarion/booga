4b8bc97d79a38ea7ebda9199be14caa67f50f60d
Luarion <raulgamallo@gmail.com>
2026-03-16 15:38:33 +0000

    Fix dotenv config path and streamline database client initialization

--- src/db/src/index.ts
+++ src/db/src/index.ts
@@ -3,15 +3,15 @@ import { resolve } from "path";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-config({ path: resolve(__dirname, "../../../../.env") });
+config({ path: resolve(__dirname, "../../../.env") });
 
-const client = postgres({
-  host: "db",
-  database: process.env.POSTGRES_DB,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-});
-
-const db = drizzle(client);
+const db = drizzle(
+  postgres({
+    host: "db",
+    database: process.env.POSTGRES_DB!,
+    user: process.env.POSTGRES_USER!,
+    password: process.env.POSTGRES_PASSWORD!,
+  }),
+);
 
 export default db;
