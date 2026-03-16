7a09f81e774ea32c4c7ab8053c54eecdb1e4d0a4
Luarion <raulgamallo@gmail.com>
2026-03-16 22:32:37 +0000

    Biome check

--- src/db/drizzle.config.ts
+++ src/db/drizzle.config.ts
@@ -1,17 +1,17 @@
+import { resolve } from "node:path";
 import { config } from "dotenv";
-import { resolve } from "path";
 import { defineConfig } from "drizzle-kit";
 
 config({ path: resolve(__dirname, "../../.env") });
 
 export default defineConfig({
-  out: "./drizzle",
-  schema: "./src/schema/index.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    host: "db",
-    user: process.env.POSTGRES_USER!,
-    password: process.env.POSTGRES_PASSWORD!,
-    database: process.env.POSTGRES_DB!,
-  },
+	out: "./drizzle",
+	schema: "./src/schema/index.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		host: "db",
+		user: process.env.POSTGRES_USER as string,
+		password: process.env.POSTGRES_PASSWORD as string,
+		database: process.env.POSTGRES_DB as string,
+	},
 });
--- src/db/scripts/seed.ts
+++ src/db/scripts/seed.ts
@@ -1,66 +1,66 @@
+import { faker as f } from "@faker-js/faker";
 import { seed } from "drizzle-seed";
 import db from "@/index";
 import * as schema from "@/schema/index";
-import { faker as f } from "@faker-js/faker";
 
 const count: number = 20;
 
 const sequentialIds = Array.from({ length: count }, (_, i) => i + 1);
 
 await seed(db, schema, { count }).refine((r) => ({
-  vehicles: {
-    columns: {
-      plate: r.valuesFromArray({
-        values: f.helpers.uniqueArray(f.vehicle.vrm, count),
-        isUnique: true,
-      }),
-      maker: r.valuesFromArray({
-        values: Array.from({ length: count }, () => f.vehicle.manufacturer()),
-      }),
-      model: r.valuesFromArray({
-        values: Array.from({ length: count }, () => f.vehicle.model()),
-      }),
-      displacement: r.valuesFromArray({
-        values: Array.from({ length: count }, () =>
-          f.number.float({ min: 0.1, max: 8.0, fractionDigits: 1 }).toString(),
-        ),
-      }),
-      fuel_consumption: r.valuesFromArray({
-        values: Array.from({ length: count }, () =>
-          f.number.float({ min: 0.1, max: 20.0, fractionDigits: 2 }).toString(),
-        ),
-      }),
-    },
-  },
-  users: {
-    columns: {
-      email: r.email(),
-      name: r.fullName(),
-      phone: r.phoneNumber(),
-    },
-  },
-  microcontrollers: {
-    columns: {
-      mac: r.valuesFromArray({
-        values: f.helpers.uniqueArray(f.internet.mac, count),
-        isUnique: true,
-      }),
-    },
-  },
-  groups_to_objects: {
-    columns: {
-      object_id: r.valuesFromArray({
-        values: sequentialIds,
-        isUnique: true,
-      }),
-    },
-  },
-  units_conversions: {
-    columns: {
-      from: r.valuesFromArray({
-        values: sequentialIds,
-        isUnique: true,
-      }),
-    },
-  },
+	vehicles: {
+		columns: {
+			plate: r.valuesFromArray({
+				values: f.helpers.uniqueArray(f.vehicle.vrm, count),
+				isUnique: true,
+			}),
+			maker: r.valuesFromArray({
+				values: Array.from({ length: count }, () => f.vehicle.manufacturer()),
+			}),
+			model: r.valuesFromArray({
+				values: Array.from({ length: count }, () => f.vehicle.model()),
+			}),
+			displacement: r.valuesFromArray({
+				values: Array.from({ length: count }, () =>
+					f.number.float({ min: 0.1, max: 8.0, fractionDigits: 1 }).toString(),
+				),
+			}),
+			fuel_consumption: r.valuesFromArray({
+				values: Array.from({ length: count }, () =>
+					f.number.float({ min: 0.1, max: 20.0, fractionDigits: 2 }).toString(),
+				),
+			}),
+		},
+	},
+	users: {
+		columns: {
+			email: r.email(),
+			name: r.fullName(),
+			phone: r.phoneNumber(),
+		},
+	},
+	microcontrollers: {
+		columns: {
+			mac: r.valuesFromArray({
+				values: f.helpers.uniqueArray(f.internet.mac, count),
+				isUnique: true,
+			}),
+		},
+	},
+	groups_to_objects: {
+		columns: {
+			object_id: r.valuesFromArray({
+				values: sequentialIds,
+				isUnique: true,
+			}),
+		},
+	},
+	units_conversions: {
+		columns: {
+			from: r.valuesFromArray({
+				values: sequentialIds,
+				isUnique: true,
+			}),
+		},
+	},
 }));
